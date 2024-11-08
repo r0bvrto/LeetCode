9c9bf8cab4d97f99f50d4c7fd8e54b07dd89be10
roberto <roberesteban839@gmail.com>
2024-11-08 10:11:48 +0100

    Quinto commit

--- src/SortColors.java
+++ src/SortColors.java
@@ -12,4 +12,9 @@ public class SortColors {
 			}
 		}
 	}
+	public static void main(String[] args) {
+		SortColors sc = new SortColors();
+		int[] tizas = {2,0,2,1,1,0};
+		System.out.println(Arrays.toString(tizas));
+	}
 }
