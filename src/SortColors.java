7f9ab3cafa828f42ebc021653e0d5a13e8454338
roberto <roberesteban839@gmail.com>
2024-11-08 10:01:02 +0100

    Tercer commit

--- src/SortColors.java
+++ src/SortColors.java
@@ -4,21 +4,12 @@ public class SortColors {
 	public void sortColors(int[] nums) {
 		for (int i = 0; i < nums.length; i++) {
 			for (int j = 0; j < nums.length - 1; j++) {
-				if (nums[j] > nums[j + 1]) {
-					int temp = nums[j];
-					nums[j] = nums[j + 1];
-					nums[j + 1] = temp;
+				
 				}
 			}
 		}
-		System.out.println(Arrays.toString(nums));
 	}
 
-	public static void main(String[] args) {
-		SortColors sc = new SortColors();
-		int[] array = { 2, 0, 2, 1, 1, 0 };
-		sc.sortColors(array);
+	
 
-	}
 
-}
