64a4fbc604a769e5761ddbc5280c86479af37357
roberto <roberesteban839@gmail.com>
2024-11-09 17:41:40 +0100

    RD commit 3

--- src/RemoveDuplicates.java
+++ src/RemoveDuplicates.java
@@ -5,14 +5,28 @@ import java.util.Set;
 
 public class RemoveDuplicates {
 	public int removeDuplicates(int[] nums) {
-		
-        ArrayList<Integer> a = new ArrayList<>(Arrays.asList(nums));
-		Set <Integer> s = new HashSet(a);
-		return s.size();
+		int index = 0;
+		for (int i = 0; i < nums.length; i++) {
+			boolean duplicado = false;
+			for (int j = i + 1; j < nums.length; j++) {
+				if (nums[i] == nums[j]) {
+
+					duplicado = true;
+					break;
+				}
+			}
+			if (!duplicado) {
+				nums[index] = nums[i];
+				index++;
+			}
+		}
+
+		return index;
 	}
+
 	public static void main(String[] args) {
 		RemoveDuplicates rd = new RemoveDuplicates();
-		int[] nums = {0,0,1,1,1,2,2,3,3,4};
+		int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
 		System.out.println(rd.removeDuplicates(nums));
 	}
 }
