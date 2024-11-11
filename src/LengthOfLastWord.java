13a4ae6d36093785996fab357cee1f7c7ffc856f
roberto <roberesteban839@gmail.com>
2024-11-11 11:52:15 +0100

    LOLW commit 2

--- src/LengthOfLastWord.java
+++ src/LengthOfLastWord.java
@@ -2,11 +2,16 @@
 public class LengthOfLastWord {
 	public int lengthOfLastWord(String s) {
 		int index = 0;
-		for(int i = 0; i < s.length(); i++) {
+		for (int i = 0; i < s.length(); i++) {
 			index++;
+			if (s.charAt(i) == ' ') {
+				if (s.charAt(s.length() - 1) != ' ') {
+				}
+			}
 		}
 		return index;
 	}
+
 	public static void main(String[] args) {
 		LengthOfLastWord lolw = new LengthOfLastWord();
 		System.out.println(lolw.lengthOfLastWord("Hello World"));
