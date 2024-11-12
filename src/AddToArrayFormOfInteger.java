9f4b588a11b1b293d5000c6548eeef761ecab60b
roberto <roberesteban839@gmail.com>
2024-11-12 20:33:35 +0100

    ATAFOI commit 3

--- src/AddToArrayFormOfInteger.java
+++ src/AddToArrayFormOfInteger.java
@@ -4,20 +4,23 @@ import java.util.List;
 public class AddToArrayFormOfInteger {
 	public List<Integer> addToArrayForm(int[] num, int k) {
 		List<Integer> a = new ArrayList<>();
-		String numero = "";
-		StringBuilder s = new StringBuilder(numero);
-		for(int i = 0; i < num.length; i++) {
+		StringBuilder s = new StringBuilder();
+		for (int i = 0; i < num.length; i++) {
 			s.append(num[i]);
 			System.out.println(s.toString());
 		}
 		int nums = Integer.parseInt(s.toString());
 		int suma = nums + k;
-		System.out.println(suma);
+		String sumaStr = Integer.toString(suma);
+		for (int i = 0; i < sumaStr.length(); i++) {
+			a.add(Character.getNumericValue(sumaStr.charAt(i)));
+		}
 		return a;
 	}
+
 	public static void main(String[] args) {
 		AddToArrayFormOfInteger ad = new AddToArrayFormOfInteger();
-		int[] num = {1,2,0,0};
+		int[] num = { 1, 2, 0, 0 };
 		System.out.println(ad.addToArrayForm(num, 34));
 	}
 }
