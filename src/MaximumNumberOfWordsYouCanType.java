4ddbf9c24058189a264938e235d47d71122a24f9
roberto <roberesteban839@gmail.com>
2024-11-13 21:13:03 +0100

    MNOWYCT commit 3

--- src/MaximumNumberOfWordsYouCanType.java
+++ src/MaximumNumberOfWordsYouCanType.java
@@ -1,12 +1,16 @@
 
 public class MaximumNumberOfWordsYouCanType {
 	public int canBeTypedWords(String text, String brokenLetters) {
+		int num = 0;
+		String[] palabras = text.split(" ");
 		for(char letra : brokenLetters.toCharArray()) {
-			if(text.contains(String.valueOf(letra))) {
-				System.out.println("contiene");
+			for(int i = 0; i < palabras.length; i++) {
+				if(palabras[i].contains(String.valueOf(letra))) {
+					num++;
+				}	
 			}
 		}
-		return 0;
+		return num;
 	}
 	public static void main(String[] args) {
 		MaximumNumberOfWordsYouCanType m = new MaximumNumberOfWordsYouCanType();
