0538c840284fd7ee89ff9595eb1b8132c4aa47be
roberto <roberesteban839@gmail.com>
2024-11-25 18:00:00 +0100

    SAOW commit 2

--- src/StringAcronymOfWords.java
+++ src/StringAcronymOfWords.java
@@ -1,19 +1,25 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class StringAcronymOfWords {
 	public boolean isAcronym(List<String> words, String s) {
-		ArrayList <String> a = new ArrayList();
-		for(int i = 0; i < s.length(); i++) {
-			System.out.println(s.charAt(i));
-			a.add(String.valueOf(s.charAt(i)));
+		if (words.size() != s.length()) {
+			return false;
 		}
-		return false;
 
+		for (int i = 0; i < words.size(); i++) {
+			if (words.get(i).charAt(0) != s.charAt(i)) {
+				return false;
+			}
+		}
+
+		return true;
 	}
+
 	public static void main(String[] args) {
 		StringAcronymOfWords saw = new StringAcronymOfWords();
-		ArrayList<String> palabras = new ArrayList();
+		ArrayList<String> palabras = new ArrayList<>(Arrays.asList("alice", "bob", "charlie"));
 		System.out.println(saw.isAcronym(palabras, "abc"));
 	}
 }
