2c555fc2ae0ede9d0d93fe669e653e5d7fbc686f
roberto <roberesteban839@gmail.com>
2024-11-26 17:25:15 +0100

    CD commit 2

--- src/ContainsDuplicate.java
+++ src/ContainsDuplicate.java
@@ -3,13 +3,12 @@ import java.util.Set;
 
 public class ContainsDuplicate {
 	public boolean containsDuplicate(int[] nums) {
+		Set <Integer> s = new HashSet();
 		for (int i = 0; i < nums.length; i++) {
-			for (int j = i + 1; j < nums.length; j++) {
-				if (nums[i] == nums[j]) {
+				if (!s.add(nums[i])) {
 					return true;
 				}
 			}
-		}
 		return false;
 	}
 
