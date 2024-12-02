161cf98fd6dd52fd38cd0ce4129c67bd7eb850f5
roberto <roberesteban839@gmail.com>
2024-12-02 20:35:12 +0100

    WPOW commit 2

--- src/WordPrefixOfWord.java
+++ src/WordPrefixOfWord.java
@@ -2,16 +2,17 @@
 public class WordPrefixOfWord {
 	public int isPrefixOfWord(String sentence, String searchWord) {
 		String[] palabras = sentence.split(" ");
-		int num = 0;
-		for(String palabra : palabras) {
-			if(palabra.contains(searchWord)) {
-				num++;
+		for (int i = 0; i < palabras.length; i++) {
+			if (palabras[i].startsWith(searchWord)) {
+				return i + 1;
 			}
 		}
-		if(num == 0) {
-			return -1;
-		}
-		return num;
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		WordPrefixOfWord wow = new WordPrefixOfWord();
+		System.out.println(wow.isPrefixOfWord("i love eating burger", "burg"));
 	}
 
 }
