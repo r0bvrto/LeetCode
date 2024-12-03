6685eebd46b369178b14c081eeeed005498c1cf8
roberto <roberesteban839@gmail.com>
2024-12-03 10:32:58 +0100

    ASTS commit 2

--- src/AddingSpacesToAString.java
+++ src/AddingSpacesToAString.java
@@ -3,17 +3,27 @@ import java.util.Arrays;
 public class AddingSpacesToAString {
 	public String addSpaces(String s, int[] spaces) {
 		StringBuilder sb = new StringBuilder();
-		for(int i = 0; i < s.length(); i++) {
+		for (int space : spaces) {
+            if (space == 0) {
+                sb.append(" ");
+                break;
+            }
+        }
+		for (int i = 0; i < s.length(); i++) {
 			sb.append(s.charAt(i));
-			if(i == spaces[i]) {
-				sb.append(" ");
+			for (int space : spaces) {
+				if (i + 1 == space) {
+					sb.append(" ");
+					break;
+				}
 			}
 		}
 		return sb.toString();
 	}
+
 	public static void main(String[] args) {
 		AddingSpacesToAString asp = new AddingSpacesToAString();
-		int[] spaces = {8,13,15};
+		int[] spaces = { 0, 8, 13, 15 };
 		System.out.println(asp.addSpaces("LeetcodeHelpsMeLearn", spaces));
 	}
 }
