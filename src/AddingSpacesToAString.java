08ac6ae9758cec61175d4ffa8196a9f31e225231
roberto <roberesteban839@gmail.com>
2024-12-03 20:10:50 +0100

    ASTS commit 4

--- src/AddingSpacesToAString.java
+++ src/AddingSpacesToAString.java
@@ -3,8 +3,7 @@ import java.util.Arrays;
 public class AddingSpacesToAString {
 	public String addSpaces(String s, int[] spaces) {
 		StringBuilder sb = new StringBuilder(s);
-		int n = spaces.length;
-		for (int i = 0; i < n; i++) {
+		for (int i = 0; i < spaces.length; i++) {
 			sb.insert(spaces[i] + i, " ");
 		}
 		return sb.toString();
