91a60c7dc49f8d346b39bdc7967b2b1e039b604f
roberto <roberesteban839@gmail.com>
2024-12-18 17:32:54 +0100

    FP commit 2

--- src/FinalPrices.java
+++ src/FinalPrices.java
@@ -4,7 +4,7 @@ public class FinalPrices {
 	public int[] finalPrices(int[] prices) {
 		for(int i = 0; i < prices.length; i++) {
 			for(int j = i + 1; j < prices.length; j++) {
-				if(prices[i] > prices[j]) {
+				if(prices[i] >= prices[j]) {
 					prices[i] -= prices[j];
 					break;
 				}
