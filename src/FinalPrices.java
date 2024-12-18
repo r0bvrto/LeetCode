d190371b8186974131f3507cd318a05f39932eb6
roberto <roberesteban839@gmail.com>
2024-12-18 17:31:14 +0100

    FP commit 1

---
+++ src/FinalPrices.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class FinalPrices {
+	public int[] finalPrices(int[] prices) {
+		for(int i = 0; i < prices.length; i++) {
+			for(int j = i + 1; j < prices.length; j++) {
+				if(prices[i] > prices[j]) {
+					prices[i] -= prices[j];
+					break;
+				}
+			}
+		}
+		return prices;
+	}
+	public static void main(String[] args) {
+		FinalPrices fp = new FinalPrices();
+		int[] prices = {8,4,6,2,3};
+		int[] resultado = fp.finalPrices(prices);
+		System.out.println(Arrays.toString(resultado));
+	}
+}
