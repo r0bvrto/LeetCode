f222b4d23b297a1de815e5add472d43bca485c38
roberto <roberesteban839@gmail.com>
2025-01-16 19:15:35 +0100

    SL commit 4

--- src/ShiftingLetters.java
+++ src/ShiftingLetters.java
@@ -3,20 +3,20 @@ public class ShiftingLetters {
 	public String shiftingLetters(String s, int[] shifts) {
 		char[] letra = s.toCharArray();
 		for (int i = 0; i < letra.length; i++) {
-			for (int j = i; j < shifts.length; j++) {
-				letra[i] += shifts[j];
-				if (letra[i] > 'z') {
-					letra[i] = (char) ('a' + (letra[i] - 'z' - 1));
-				}
+			int shift = shifts[i % shifts.length] % 26;
+
+			letra[i] += shift;
+
+			if (letra[i] > 'z') {
+				letra[i] = (char) ('a' + (letra[i] - 'z' - 1));
 			}
 		}
-		String palabra = new String(letra);
-		return palabra;
+		return new String(letra);
 	}
 
 	public static void main(String[] args) {
 		ShiftingLetters sl = new ShiftingLetters();
-		int[] shifts = {52};
+		int[] shifts = { 52 };
 		System.out.println(sl.shiftingLetters("z", shifts));
 
 	}
